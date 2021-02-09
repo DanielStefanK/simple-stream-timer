95e41650dc805b86bd1d0eb52f4e14b6a64a5333
Daniel Stefan Klose <daniel-stefan.klose@udo.edu>
2021-02-09 12:54:55 +0100

    added basic timer for obs

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,54 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1 @click="changeState" :class="{alert: currentTime < 1 && state != 0, stopped: state==0 }">{{prettyTime}}</h1>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { computed, ref } from 'vue'
+const sound = require('@/assets/chime.mp3');
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  setup () {
+    const config = getConfig ()
+    // 0 is stopped 1 is running
+    const state = ref(0)
+    const index = ref(0)
+    const currentTime = ref (0)
+    let countdownInterval;
+    var audio = new Audio(sound)
+
+
+    const changeState = ()=>{
+      //is running
+      if (state.value) {
+        state.value = 0
+        currentTime.value = config[index.value%config.length]
+        clearInterval(countdownInterval)
+      } else {
+        state.value = 1
+        const seconds = config[index.value%config.length]
+        index.value++
+        currentTime.value = seconds
+        countdownInterval =  setInterval(()=>{
+          currentTime.value--
+          if (currentTime.value === 0) audio.play()
+        }, 1000)
+      }
+    }
+
+    const prettyTime = computed(()=>{
+      const seconds =  Math.abs(currentTime.value % 60)
+      const minutes = Math.abs(Math.[currentTime.value < 0 ?'round' : 'floor'](currentTime.value / 60))
+      return `${currentTime.value<0?'-':''}${minutes>9?'':'0'}${minutes}:${seconds>9?'':'0'}${seconds}`
+    })
+
+    return {prettyTime, changeState, currentTime, state}
   }
+}
+
+const getConfig = () => {
+    const urlParams = new URLSearchParams(window.location.search);
+    return JSON.parse (urlParams.get('config'));
 }
 </script>
 
@@ -22,5 +60,15 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+h1 {
+  font-size: 500%;
+}
+.alert {
+  color: rgb(242, 115, 115);
+}
+
+.stopped {
+  color: rgb(115, 230, 172);
 }
 </style>
