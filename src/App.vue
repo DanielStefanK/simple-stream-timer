228425ff56a1c32ee059fb580999e629f15fbfef
Daniel Stefan Klose <daniel-stefan.klose@udo.edu>
2021-02-11 19:43:46 +0100

    added animations

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
-  <div @click="changeState" class="covercard">
-      <h1 :class="{alert: currentTime < 1 && state != 0, stopped: state==0 }">{{prettyTime}}</h1>
+  <div @click="changeState" :class="{covercard: true, shakecard: currentTime <=0 && currentTime >-1 && state!=0}">
+      <h1 :class="{alert: currentTime < 1 && state != 0, stopped: state==0, blinking: currentTime <= -30, pulsing: currentTime <= -60 }">{{prettyTime}}</h1>
   </div>
 </template>
 
@@ -86,5 +86,75 @@ h1 {
   box-shadow: 14px 24px 24px 5px rgba(0,0,0,0.23);
   width: 300px;
   cursor: pointer;
+}
+
+.shakecard {
+  /* Start the shake animation and make the animation last for 0.5 seconds */
+  animation: shake .5s;
+
+  /* When the animation is finished, start again */
+  animation-iteration-count: once;
+}
+
+.blinking {
+  /* Start the shake animation and make the animation last for 0.5 seconds */
+  animation: blink .7s;
+
+  /* When the animation is finished, start again */
+  animation-iteration-count: infinite;
+}
+
+.pulsing {
+  /* Start the shake animation and make the animation last for 0.5 seconds */
+  animation: pulse 1s;
+
+  /* When the animation is finished, start again */
+  animation-iteration-count: infinite;
+}
+
+@keyframes shake {
+  10%, 90% {
+    transform: translate3d(-1px, 0, 0);
+  }
+  
+  20%, 80% {
+    transform: translate3d(2px, 0, 0);
+  }
+
+  30%, 50%, 70% {
+    transform: translate3d(-4px, 0, 0);
+  }
+
+  40%, 60% {
+    transform: translate3d(4px, 0, 0);
+  }
+}
+
+
+@keyframes blink {
+  0% {
+    opacity: 100%;
+  }
+  30% {
+    opacity: 0%;
+  }
+  70% {
+    opacity: 100%;    
+  }
+  100%{
+    opacity: 100%;
+  }
+}
+
+@keyframes pulse {
+  0% {
+    transform: scale(1);
+  }
+  50% {
+    transform: scale(1.2);
+  }
+  100%{
+    transform: scale(1);
+  }
 }
 </style>
